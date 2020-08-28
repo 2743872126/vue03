9d20643ea7e22a1612e566210deedc56b94717f1
qianliuzhu939495 <3111801255@qq.com>
2020-08-28 21:24:37 +0800

    sdfg

--- src/components/cookbook/createMenus.vue
+++ src/components/cookbook/createMenus.vue
@@ -1,56 +1,56 @@
 <template>
-   <div class="main">
-     <div style="float:left;width: 60%">
-       <el-form :model="newMenu" status-icon :rules="rules" ref="mform" label-width="100px" class="demo-ruleForm">
-         <el-form-item prop="mname">
-           <input style="width: 100%;height: 60px;font-size: 55px;border: 1px solid gainsboro ;" v-model="newMenu.mname" placeholder="请输入菜谱名"></input>
-         </el-form-item>
-         <el-form-item style="text-align: left" >
-           <el-upload list-type="picture-card" ref="upload" drag="" accept=".jpg,.png" action="http://localhost:8080/cookbooktest/file/uploadImage" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :on-change="handlechange" :auto-upload="false" limit="1" :file-list="filelist">
-             <el-link style="font-size: 22px" slot="trigger" size="small" type="primary">上传预览图</el-link>
-           </el-upload>
-           <el-dialog :visible.sync="dialogVisible">
-             <img width="100%" :src="tu1"  alt="">
-           </el-dialog>
-         </el-form-item>
-         <el-form-item prop="Info" style="margin-top: 60px">
-           <el-input style="width: 100%;font-size: 33px;" v-model="newMenu.Info" type="textarea" placeholder="点击添加菜谱描述"></el-input>
-         </el-form-item>
-         <el-form-item>
-           <h1 style="text-align: left;margin-bottom: 10px">用料  :</h1>
-           <el-row :gutter="10" v-for="(v,i) in detail">
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="1" style="font-size: 32px">{{i+1}}.</el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><input v-model="v.Material" class="inputs"></input></el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="10"><input v-model="v.Num" class="inputs"></input></el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="2"><icon  @click="removeAppointUser(i)" style="font-size: 40px;margin-top: 4px" class="el-icon-close"></icon></el-col>
-           </el-row>
-           <el-button @click="addAppointUser" round style="float:left;background-color: crimson;color: white; font-size: 30px">追加一行</el-button>
-         </el-form-item>
-         <el-form-item>
-           <h1 style="text-align: left;margin-bottom: 10px">做法步骤  :</h1>
-           <el-row :gutter="10" v-for="(v,i) in menuSteps">
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="1" style="font-size: 32px">{{i+1}}.</el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><el-input type="textarea" style="margin-bottom: 10px;font-size: 33px"  rows="5" v-model="v.msinfo" class="inputsplus"></el-input></el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="10">
-               <el-upload list-type="picture-card" ref="upload"  accept=".jpg,.png" action="http://localhost:8080/cookbooktest/file/uploadImage" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :on-change="handlechange" :auto-upload="false" limit="1" :file-list="filelist">
-                 <el-link style="font-size: 22px" slot="trigger" size="small" type="primary">上传步骤图</el-link>
-               </el-upload>
-               <el-dialog :visible.sync="dialogVisible">
-                 <img width="100%" :src="tu1"  alt="">
-               </el-dialog>
-             </el-col>
-             <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="2"><icon  @click="removestep(i)" style="font-size: 40px;margin-top: 4px" class="el-icon-close"></icon></el-col>
-           </el-row>
-           <el-button @click="addstep" round style="float:left;background-color: crimson;color: white; font-size: 30px">追加一个步骤</el-button>
-         </el-form-item>
-         <el-form-item>
-           <el-button type="primary" @click="submitForm">发布菜谱</el-button>
-           <el-button @click="resetForm">存草稿</el-button>
-         </el-form-item>
-       </el-form>
-     </div>
-     <div></div>
-   </div>
+  <div class="main">
+    <div style="float:left;width: 60%">
+      <el-form :model="newMenu" status-icon :rules="rules" ref="mform" label-width="100px" class="demo-ruleForm">
+        <el-form-item prop="mname">
+          <input style="width: 100%;height: 60px;font-size: 55px;border: 1px solid gainsboro ;" v-model="newMenu.mname" placeholder="请输入菜谱名"></input>
+        </el-form-item>
+        <el-form-item style="text-align: left" >
+          <el-upload list-type="picture-card" ref="upload" drag="" accept=".jpg,.png" action="http://localhost:8080/cookbooktest/file/uploadImage" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :on-change="handlechange" :auto-upload="false" limit="1" :file-list="filelist">
+            <el-link style="font-size: 22px" slot="trigger" size="small" type="primary">上传预览图</el-link>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="tu1"  alt="">
+          </el-dialog>
+        </el-form-item>
+        <el-form-item prop="Info" style="margin-top: 60px">
+          <el-input style="width: 100%;font-size: 33px;" v-model="newMenu.Info" type="textarea" placeholder="点击添加菜谱描述"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <h1 style="text-align: left;margin-bottom: 10px">用料  :</h1>
+          <el-row :gutter="10" v-for="(v,i) in detail">
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="1" style="font-size: 32px">{{i+1}}.</el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><input v-model="v.Material" class="inputs"></input></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="10"><input v-model="v.Num" class="inputs"></input></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="2"><icon  @click="removeAppointUser(i)" style="font-size: 40px;margin-top: 4px" class="el-icon-close"></icon></el-col>
+          </el-row>
+          <el-button @click="addAppointUser" round style="float:left;background-color: crimson;color: white; font-size: 30px">追加一行</el-button>
+        </el-form-item>
+        <el-form-item>
+          <h1 style="text-align: left;margin-bottom: 10px">做法步骤  :</h1>
+          <el-row :gutter="10" v-for="(v,i) in menuSteps">
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="1" style="font-size: 32px">{{i+1}}.</el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><el-input type="textarea" style="margin-bottom: 10px;font-size: 33px"  rows="5" v-model="v.msinfo" class="inputsplus"></el-input></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="10">
+              <el-upload list-type="picture-card" ref="upload"  accept=".jpg,.png" action="http://localhost:8080/cookbooktest/file/uploadImage" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :on-change="handlechange" :auto-upload="false" limit="1" :file-list="filelist">
+                <el-link style="font-size: 22px" slot="trigger" size="small" type="primary">上传步骤图</el-link>
+              </el-upload>
+              <el-dialog :visible.sync="dialogVisible">
+                <img width="100%" :src="tu1"  alt="">
+              </el-dialog>
+            </el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="2"><icon  @click="removestep(i)" style="font-size: 40px;margin-top: 4px" class="el-icon-close"></icon></el-col>
+          </el-row>
+          <el-button @click="addstep" round style="float:left;background-color: crimson;color: white; font-size: 30px">追加一个步骤</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">发布菜谱</el-button>
+          <el-button @click="resetForm">存草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <script>
@@ -122,7 +122,7 @@
       },
     }
 
-}
+  }
 </script>
 
 <style scoped>
