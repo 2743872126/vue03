162d4304687b07308dbfc6c6b6758b8c7ffac26b
qianliuzhu939495 <3111801255@qq.com>
2020-08-31 21:09:53 +0800

    sdfg

---
+++ src/components/cookbook/Tools.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      个人中心
+      <el-link @click="gets">微博登录</el-link>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Tools',
+    methods:{
+      gets(){
+        this.$axios.post("http://127.0.0.1:8080/cookbooktest/Weibo/test")
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
